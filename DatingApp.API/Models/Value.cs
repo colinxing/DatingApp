8c6459c0cd34106850bddfd51de14ed251ad70ce
System Administrator <root@XiaoquandeMacBook-Pro.local>
2018-09-27 17:07:10 -0500

    Initial Commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        // Able to get and set data from other class
+        public int Id { get;set; }
+        
+        public string Name { get; set; }
+    }
+}
